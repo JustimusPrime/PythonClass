b98ca8664244b5258ede56d1196347f8a2d041f1
JustimusPrime <hirejustin@yahoo.com>
2018-06-04 13:03:04 -0600

    first version of converter

---
+++ converter.py
@@ -0,0 +1,4 @@
+print("How many miles did you want to convert into kilometers?")
+miles = int(input())
+kilometers = miles * 1.60934
+print(f"{miles} miles would convert to {kilometers} km")
---
+++ numbersAndMath/comments.py
@@ -0,0 +1,9 @@
+print(1/2 * 2)
+#this is a comment
+#technically this # is called an octothorp
+# jhjhv
+# ui
+# when to use comments? Can do it a lot for code you're reading, for production you don't want to overdo it, your code should be readable enough to not need it
+#corporate philosophy 
+
+# 1/2 
